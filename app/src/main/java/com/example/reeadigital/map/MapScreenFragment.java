d4c1e4aaed5a7b2aa6c12705dd56a1b2ee7c9586
sadid7 <sadid7@live.com>
2022-11-18 09:14:27 +0600

    adds landscape orientation implementation

--- app/src/main/java/com/example/reeadigital/map/MapScreenFragment.java
+++ app/src/main/java/com/example/reeadigital/map/MapScreenFragment.java
@@ -4,10 +4,10 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -21,60 +21,64 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-import okhttp3.internal.Util;
-
-public class MapScreenFragment extends SupportMapFragment implements OnMapReadyCallback,OnSuccessListener<Location> {
-    private GoogleMap mMap;
+public class MapScreenFragment extends Fragment implements OnSuccessListener<Location>,OnMapReadyCallback{
+    private MapView googleMapView;
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
 
-    GoogleApiClient mGoogleApiClient;
-    LocationRequest mLocationRequest;
-
-
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    @NonNull
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         View view = (View) inflater.inflate(R.layout.fragment_map_screen_layout, container, false);
-
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        googleMapView = view.findViewById(R.id.mv_google_map);
+        googleMapView.onCreate(savedInstanceState);
+        //googleMapView.onResume();
         fusedLocationProviderClient = LocationServices
                 .getFusedLocationProviderClient(getContext());
         fetchLastLocation();
-        //return super.onCreateView(inflater, container, savedInstanceState);
+      /*  try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+        //googleMapView.getMapAsync(this);
         return view;
     }
 
-
     private void fetchLastLocation() {
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext()
+                        , Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(getActivity(), new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION}, Utils.REQUEST_CODE);
             return;
         }
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        Log.e("Sabid", "FetchLast");
+
         task.addOnSuccessListener(this);
 
     }
 
     @Override
-    public void onSuccess(Location location) {
-
-        if (location != null) {
-            currentLocation = location;
-            getMapAsync(this);
-        }
-    }
-
-    @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        Log.e("Sabid", "OnMapReady");
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng)
                 .title("You Are Here!");
@@ -92,5 +96,14 @@ public class MapScreenFragment extends SupportMapFragment implements OnMapReadyC
                 }
                 break;
         }
+    }
+
+    @Override
+    public void onSuccess(Location location) {
+        if (location != null) {
+            currentLocation = location;
+            googleMapView.getMapAsync(this);
+        }
+        googleMapView.onResume();
     }
 }
